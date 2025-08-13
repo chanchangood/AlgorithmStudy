dd785e58910a5be896f7628a97f755ce056e9488
chanchangood <chanwoo.kim31@gmail.com>
2025-08-13 23:31:57 +0900

    13913-숨박꼭질4

--- BFS/13913-숨박꼭질4.java
+++ BFS/13913-숨박꼭질4.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int n, k;
 	public static void main(String[] args) throws Exception {
 		solution();
 	}
@@ -12,11 +13,19 @@ public class Main {
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
+			// 0 <= n,k <= 100,000
+			n = Integer.parseInt(st.nextToken()); // 시작 위치
+			k = Integer.parseInt(st.nextToken()); // 목적지
+
 
 
 			bw.flush();
 		}catch (IOException e){
 			e.printStackTrace();
 		}
+	}
+
+	static int bfs(){
+
 	}
 }
